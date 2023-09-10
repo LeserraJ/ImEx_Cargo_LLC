4ca1a8d9fccc6a4bce158f5759948f2d47a958e4
John Leserra <leserrabusiness@gmail.com>
2023-09-09 23:55:08 -0400

    added main content

--- src/app/header/components/Nav.tsx
+++ src/app/header/components/Nav.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import Image from "next/image";
 
 export default function NavBar() {
   return (
@@ -45,10 +44,10 @@ export default function NavBar() {
                 Home
               </Link>
             </li>
-            <li>
+            <li className=" ">
               <Link
                 href="/tracking"
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent "
               >
                 Tracking
               </Link>
@@ -73,5 +72,6 @@ export default function NavBar() {
         </div>
       </div>
     </nav>
+    
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,16 @@ import HeaderMain from "./header/Header";
 
 
 export default function Home() {
-  <head>
-<link rel="icon" href="/favicon.ico" sizes="any" />
-</head>
   return (
-   
-   
-  
-  
-  
-  
-  
-  
-  
-  <HeaderMain />
+    <body>
+      <HeaderMain />
+
+      <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">We invest in the world’s potential</h1>
+<p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-black">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+<button className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+   Get Quote
+</button>
+
+    </body>
   );
 }
